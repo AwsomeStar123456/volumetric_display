51e5fe0261428686e62ee09ab833a9c2a2734b62
Semrah Odobasic <semrahodobasic@icloud.com>
2024-04-04 18:35:30 -0600

    Created display methods to allow numbers to be displayed. Got the IR sensor working with the GPIO. Whenever a falling edge is detected we calculate the rpm based on the last time the method was called.

--- Code-Testing/Motor/display.c
+++ Code-Testing/Motor/display.c
@@ -3,24 +3,38 @@
 #include <string.h>
 
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/time.h"
 #include "hardware/pio.h"
 #include "hardware/i2c.h"
 #include "hardware/clocks.h"
 #include "hardware/pwm.h"
-#include "pico/time.h"
 #include "IS31FL3731.h"
 
 #define MTR_LEFT 0
 #define MTR_RIGHT 1
+
+#define RPM_AVERAGE_COUNT 5
+
+volatile uint32_t last_times[RPM_AVERAGE_COUNT] = {0};
+volatile uint32_t rpm = 0;
+volatile uint32_t rpm_index = 0;
 
 
 void init_i2c();
 void init_motor();
 void set_motor_pwm(bool direction, uint8_t speed);
 void stop_motor();
+void init_ir_sense();
 void person();
+void display_numbers(int numbers[4]);
+void display_number(int number, int position, int frame);
 
-uint8_t frame;
+void calc_rpm(uint gpio, uint32_t events);
+
+volatile uint8_t frame;
+volatile bool core1_uninitialized = true;
+volatile bool rpm_updated = false;
 
 /*
     This method initializes the I2C bus for pins 14 and 15 of the pico.
@@ -52,6 +66,52 @@ void init_motor()
     gpio_put(11, 1);
 }
 
+bool timer_callback(repeating_timer_t *rt) {
+    if (!rpm_updated) {
+        // No falling edge detected since last update, set RPM to zero
+        rpm = 0;
+    }
+
+    rpm_updated = false;
+
+    // Must return true to keep the timer repeating
+    return true;
+}
+
+void init_timer() {
+    // Set up our repeating timer
+    static repeating_timer_t timer;
+    add_repeating_timer_ms(1000, timer_callback, NULL, &timer);
+}
+
+//Read from time and set the global RPM variable
+void calc_rpm(uint gpio, uint32_t events)
+{
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    if (last_times[rpm_index] != 0) {
+        // Calculate the time for one revolution (in microseconds)
+        uint32_t revolution_time = current_time - last_times[rpm_index];
+
+        // Calculate RPM: 60 seconds per minute, 1e6 microseconds per second
+        rpm = 60 * 1e6 / revolution_time;
+    }
+
+    last_times[rpm_index] = current_time;
+    rpm_index = (rpm_index + 1) % RPM_AVERAGE_COUNT;
+    rpm_updated = true;
+}
+
+
+void init_ir_sense()
+{
+    // IR Sensor
+    gpio_init(26);
+    gpio_set_dir(26, GPIO_IN);
+
+    gpio_set_irq_enabled_with_callback(26, GPIO_IRQ_EDGE_FALL, true, &calc_rpm);
+}
+
 void set_motor_pwm(bool direction, uint8_t speed) // Speed in terms of percentage
 {
     // Convert speed in percentage to 16-bit value
@@ -75,31 +135,67 @@ void stop_motor()
     pwm_set_gpio_level(12, 0);
 }
 
+void core1_entry() {
+    //Initalize
+    init_ir_sense();
+    init_timer();
+
+
+    core1_uninitialized = false;
+    while (1) {
+
+        set_motor_pwm(MTR_LEFT, 100);
+        // sleep_ms(2000);
+        // stop_motor();  
+        // sleep_ms(2000);
+        // set_motor_pwm(MTR_RIGHT, 100);
+        // sleep_ms(2000);
+        // stop_motor();
+        // sleep_ms(2000);
+
+    }
+}
+
 int main()
 {
+    //Initialize
     stdio_init_all();
     init_i2c();
     init_ledDisplay(0);
     init_motor();
-
     clear(i2c1, ISSI_ADDR_DEFAULT, frame);
-    person();
+    //person();
+
+    //Launch Core 1
+    multicore_launch_core1(core1_entry);
+    while(core1_uninitialized) {}
 
     while (1) {
         // //For Person
-          displayFrame(i2c1, ISSI_ADDR_DEFAULT, 0);
-         sleep_ms(1000);
-          displayFrame(i2c1, ISSI_ADDR_DEFAULT, 1);
-          sleep_ms(1000);
+        // displayFrame(i2c1, ISSI_ADDR_DEFAULT, 0);
+        // sleep_ms(500);
+        // displayFrame(i2c1, ISSI_ADDR_DEFAULT, 1);
+        // sleep_ms(500);
 
-        // set_motor_pwm(MTR_LEFT, 100);
-        // sleep_ms(5000);
-        // stop_motor();  
-        // sleep_ms(5000);
-        // set_motor_pwm(MTR_RIGHT, 100);
-        // sleep_ms(5000);
-        // stop_motor();
-        // sleep_ms(5000);
+
+        // if(rpm > 500) {
+        //     displayFrame(i2c1, ISSI_ADDR_DEFAULT, 0);
+        // } else {
+        //     displayFrame(i2c1, ISSI_ADDR_DEFAULT, 1);
+        // }
+
+        clear(i2c1, ISSI_ADDR_DEFAULT, frame);
+
+        // Convert RPM to array of digits
+        int numbers[4] = {0, 0, 0, 0};  // Initialize to 0000
+        int temp_rpm = rpm;
+        for (int i = 3; i >= 0 && temp_rpm > 0; i--) {
+            numbers[i] = temp_rpm % 10;
+            temp_rpm /= 10;
+        }
+
+        display_numbers(numbers);
+        sleep_ms(1000);
     }
 
     return 0;
@@ -214,4 +310,206 @@ void person(){
     setPixel(i2c1, ISSI_ADDR_DEFAULT, 3, 6, 100, frame);
     setPixel(i2c1, ISSI_ADDR_DEFAULT, 4, 6, 100, frame);
     setPixel(i2c1, ISSI_ADDR_DEFAULT, 14, 6, 100, frame);
+}
+
+/*int digits[10][7][4] = {
+    {
+        {0, 1, 1, 0},  // 0
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 0, 1, 0},  // 1
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 1, 1, 1}
+    },
+    {
+        {0, 1, 1, 0},  // 2
+        {1, 0, 0, 1},
+        {0, 0, 1, 0},
+        {0, 1, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 1, 1, 1}
+    },
+    {
+        {1, 1, 1, 0},  // 3
+        {0, 0, 0, 1},
+        {0, 0, 0, 1},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 1},
+        {1, 1, 1, 0}
+    },
+    {
+        {0, 0, 1, 1},  // 4
+        {0, 1, 0, 1},
+        {1, 0, 0, 1},
+        {1, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 1}
+    },
+    {
+        {0, 1, 1, 0},  // 5
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 1},
+        {1, 0, 0, 1},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 1, 1, 0},  // 6
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {0, 1, 1, 0},
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {0, 1, 1, 0}
+    },
+    {
+        {1, 1, 1, 1},  // 7
+        {0, 0, 0, 1},
+        {0, 0, 1, 0},
+        {0, 1, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0}
+    },
+    {
+        {0, 1, 1, 0},  // 8
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {0, 1, 1, 0},
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {0, 1, 1, 0}
+    },
+    {
+        {0, 1, 1, 0},  // 9
+        {1, 0, 0, 1},
+        {1, 0, 0, 1},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 0}
+    }
+};*/
+
+int digits[10][7][4] = {
+    {
+        {0, 0, 0, 0},  // 0
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 1, 0, 1},
+        {0, 1, 0, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},  // 1
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0}
+    },
+    {
+        {0, 0, 0, 0},  // 2
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 1, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},  // 3
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 1, 1},
+        {0, 0, 0, 1},
+        {0, 1, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},  // 4
+        {0, 0, 0, 0},
+        {0, 1, 0, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 1}
+    },
+    {
+        {0, 0, 0, 0},  // 5
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 1, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 1, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},  // 6
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},  // 7
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 1, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0}
+    },
+    {
+        {0, 0, 0, 0},  // 8
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 1}
+    },
+    {
+        {0, 0, 0, 0},  // 9
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 1}
+    }
+};
+
+void display_number(int number, int position, int frame) {
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (digits[number][i][j]) {
+                setPixel(i2c1, ISSI_ADDR_DEFAULT, position + j, i + 1, 100, frame);
+            }
+        }
+    }
+}
+
+void display_numbers(int numbers[4]) {
+    for (int i = 0; i < 4; i++) {
+        display_number(numbers[i], 4 * i, frame);
+    }
 }
