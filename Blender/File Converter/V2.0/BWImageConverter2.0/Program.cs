cd8d5c48ad2d7aca382ea3238f395902b40f5bc9
Semrah Odobasic <semrahodobasic@icloud.com>
2024-07-16 22:13:05 -0600

    Worked on the new chassis and worked on the new C# conversion script. Tomorrow we meet in person and work on assembly and finishing the conversion code.

---
+++ Blender/File Converter/V2.0/BWImageConverter2.0/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConverterCode;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+MyImageConverter converter = new MyImageConverter();
+converter.DirectoryImagesToTextFile("../../../../../../!Volumetric Blender Data/Cube/Render Data", "../../../../../../!Volumetric Blender Data/Cube/Text Data/Output.txt");
---
+++ Blender/File Converter/V2.0/ConverterCode/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace ConverterCode
+{
+    public class MyImageConverter
+    {
+        public void DirectoryImagesToTextFile(string inputPath, string outputPath)
+        {
+            using (StreamWriter writer = new StreamWriter(outputPath))
+            {
+                string[] imagePaths = Directory.GetFiles(inputPath, "*.png");
+
+                foreach (string imagePath in imagePaths)
+                {
+                    ImageToTextFile(imagePath, writer);
+                }
+            }
+        }   
+
+        public void ImageToTextFile(string inputPath, StreamWriter writer)
+        {
+            using (Bitmap image = new Bitmap(inputPath))
+
+                for (int x = 0; x < image.Width; x++)
+                {
+                    string binaryColumn = "";
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Color pixelColor = image.GetPixel(x, y);
+                        // Assuming white is represented as 255,255,255 and black as 0,0,0
+                        // You might need to adjust the threshold based on your image
+                        binaryColumn += pixelColor.GetBrightness() >= 0.5 ? "1" : "0";
+                    }
+                    writer.WriteLine(binaryColumn);
+                }
+
+
+
+        }
+
+
+
+    }
+}
