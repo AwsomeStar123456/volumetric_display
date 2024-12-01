bddc757a4ee60c633e9de86be1b70fb1883c3b03
Semrah Odobasic <semrahodobasic@icloud.com>
2024-12-01 11:55:47 -0700

    Implemented animation switching & commented code.

--- Code-Testing/!DisplayGPIO/test/displayPIO.c
+++ Code-Testing/!DisplayGPIO/test/displayPIO.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Raspbery Pi RP2040 Libraries
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/time.h"
@@ -15,6 +16,7 @@
 #include "hardware/gpio.h"
 #include "hardware/timer.h"
 
+//Our Custom Libraries
 #include "imagedata.h"
 #include "OriginalImageData.h"
 #include "GpioMapping.h"
@@ -22,48 +24,61 @@
 #include "Initialization.h"
 #include "Motor.h"
 
+//Base Constants
+#define TARGET_RPM 1736 //Target RPM for the motor. 1736 RPM = 29 FPS (Frames Per Second)
+#define TIME_UNTIL_SHUTDOWN 3000000 //Time in us = 3 Seconds | Sets the time until the motor shuts down after the last IR sensor reading.
 
-#define TARGET_RPM 1736 //Target RPM for the motor
-#define TIME_UNTIL_SHUTDOWN 3000000 //Time in us = 3 Seconds
+#define PIXEL_TIME 10 //DO NOT EDIT! Time in us that each pixel is displayed for.
+#define NUM_3D_FRAMES 168 //Number of 3D frames we have in the animation.
+#define FRAMES_BEFORE_NEXT 1 //Number of times we repeat a frame before moving to the next 3D frame. Allows for slower FPS.
+
+//PID Constants
 #define Kp 0.05f // Proportional constant
 #define Ki 0.1f // Integral constant
 #define Kd 0.01f // Derivative constant
 float lastError = 0.0f;
 volatile int pwm = 0;
 
-#define PIXEL_TIME 10 //Time in us that each pixel is displayed for.
-#define NUM_3D_FRAMES 88 //Number of 3D frames we have in the animation.
-#define FRAMES_BEFORE_NEXT 3 //Number of times we repeat a frame before moving to the next 3D frame.
-
+//Motor & IR Settings
+//Shouldn't be touched. This part deals with the noise from the IR sensor.
 #define VOLTAGE_THRESHOLD 2 // Voltage threshold in volts
 #define ADC_PIN 28 // GPIO pin connected to the ADC
 #define IR_SENSOR_PIN 28 // GPIO pin connected to the IR sensor
 #define ADC_CHANNEL 2 // ADC channel corresponding to GPIO28
 #define DEBOUNCE_TIME_MS 25 // Debounce time in milliseconds
 
-
-volatile int pulse_count = 0;
-volatile bool previous_below_threshold = false;
-
+//Onboard LED Settings
 #define WS2812_PIN 16
 #define NUM_PIXELS 1
 #define IS_RGBW true
 
-//Set the pixel color values for easy access.
+//Set the LED color values for easy access.
 const uint32_t ws2812Red   = 0x00FF0000; // RGB format: 0x00RRGGBB
 const uint32_t ws2812Green = 0x0000FF00; // RGB format: 0x00RRGGBB
 const uint32_t ws2812Blue  = 0x000000FF; // RGB format: 0x00RRGGBB
 const uint32_t ws2812Black = 0x00000000; // RGB format: 0x00RRGGBB
 
-volatile bool core1_uninitialized = true;
-bool atTargetRPM = false;
-int currentRPM = 0;
-absolute_time_t lastTime, currentTime;
-bool is_captured;
+//Animation Switching Variables
+#define NUM_ANIMATIONS 3 //Number of animations we have in the animation array.
+int animations_currentframe = 0; //Which animation to start on.
 
-void clearLEDs();
-void calc_rpm(uint gpio, uint32_t events);
+uint16_t (*animations[NUM_ANIMATIONS])[24][9] = {water, intel, rotatingcube}; //Array of animations.
+uint16_t animations_framecount[NUM_ANIMATIONS] = {NUM_WATER_FRAMES, NUM_INTEL_FRAMES, NUM_ROTATINGCUBE_FRAMES}; //Number of frames in each animation.
+uint16_t animations_framebeforenext[NUM_ANIMATIONS] = {1, 3, 2}; //Number of frames to repeat before moving to the next frame.
+
+uint16_t (*activeFrame)[24][9]; 
+
+//Global Variables
+volatile bool core1_uninitialized = true;
+int currentRPM = 0;
+bool atTargetRPM = false;
+absolute_time_t lastTime, currentTime;
+
+//Helper Methods
 void set_onboard_led_color(PIO pio, int sm, uint32_t color);
+float read_voltage();
+void calc_rpm(uint gpio, uint32_t events);
+void clearLEDs();
 
 /*
     This method sets the color of the onboard LED.
@@ -134,24 +149,23 @@ void core1_entry() {
     sleep_ms(7500); //Sleep to allow user to plug in USB and open serial connection.
     printf("Initiating Core1 Setup.\n");
 
+    //Start up the ADC.
     printf("Initiating ADC.\n");
     adc_init();
     adc_gpio_init(ADC_PIN);
     adc_select_input(ADC_CHANNEL);
 
+    //Set up the GPIO for the IR sensor.
     printf("Initiating IR Sensor Pin.\n");
     gpio_init(28);
     gpio_set_dir(28, GPIO_IN);
     gpio_pull_up(28);
 
+    //Set up the motor.
     printf("Initiating Motor.\n");
     init_motor();
 
-    // Allow time for the motor to start
-    //set_motor_pwm(100);
-    //sleep_ms(3000);
-
-    // Initialize the PIO and state machine. This is the PIO for the WS2812 LED.
+    //Initialize the PIO and state machine. This is the PIO for the WS2812 LED.
     printf("Starting LED.\n");
     PIO pio = pio0;
     int sm = 0;
@@ -159,7 +173,7 @@ void core1_entry() {
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
     set_onboard_led_color(pio, sm, 0x00111111); // Set LED to white
 
-    // Report to core0 that it can start processing as core 1 has initialized its peripherals.
+    //Report to core0 that it can start processing as core 1 has initialized its peripherals.
     core1_uninitialized = false;
     bool motorEnabled = false;
     bool motorIRQ = false;
@@ -168,11 +182,8 @@ void core1_entry() {
 
     sleep_ms(1000);
 
-    // Set up the interrupt for the IR sensor.
-    //gpio_set_irq_enabled_with_callback(28, GPIO_IRQ_EDGE_FALL, true, &calc_rpm);
-
     while(true) {
-        printf("Doing Loop\n");
+        //Check if the motor should be enabled or disabled. This is based on the time since the last IR sensor reading and current system time.
         if(to_us_since_boot(get_absolute_time()) - to_us_since_boot(lastTime) > TIME_UNTIL_SHUTDOWN || to_ms_since_boot(get_absolute_time()) < 3000) {
             motorEnabled = false;
 
@@ -181,6 +192,7 @@ void core1_entry() {
             }
         } 
 
+        //If the motor is enabled, set the PWM to 100 and start the motor IRQ.
         if(motorEnabled) {
             if(!motorIRQ) {
                 set_motor_pwm(100);
@@ -190,6 +202,7 @@ void core1_entry() {
                 motorIRQ = true;
             }
 
+            //Set the onboard LED color based on the current RPM.
             if(currentRPM >= TARGET_RPM + 50){
                 set_onboard_led_color(pio, sm, ws2812Blue);
                 atTargetRPM = false;
@@ -202,14 +215,17 @@ void core1_entry() {
             }
 
         } else {
+            //If the motor is disabled, set the PWM to 0 and disable the motor IRQ. Also set the onboard LED to black. Increment to next animation frame.
             if(motorIRQ) {
                 set_motor_pwm(0);
                 gpio_set_irq_enabled_with_callback(28, GPIO_IRQ_EDGE_FALL, false, NULL);
                 motorIRQ = false;
+                animations_currentframe++;
             }
             atTargetRPM = false;
             set_onboard_led_color(pio, sm, ws2812Black);
         }
+
         sleep_ms(500);
     }
 }
@@ -223,7 +239,7 @@ int main() {
     stdio_init_all();
     init_led();
 
-    //Launch Core 1
+    //Launch Core 1 & Wait for it to initialize its peripherals.
     multicore_launch_core1(core1_entry);
     while(core1_uninitialized) {}
 
@@ -233,17 +249,24 @@ int main() {
     int currentGND = 0;
     bool pixelVal = 0;
 
+    // Animation Switching Variables
+    int current_animations_framebeforenext = 0;
+    uint16_t animations_numframes;
+
     while (1) {
 
-        // TODO - Replace true with atTargetRPM
-        //atTargetRPM
+
         if(atTargetRPM) {
+            //Get the current animation frame and its respective frame count and speed.
+            activeFrame = animations[animations_currentframe % NUM_ANIMATIONS];
+            animations_numframes = animations_framecount[animations_currentframe % NUM_ANIMATIONS];
+            current_animations_framebeforenext = animations_framebeforenext[animations_currentframe % NUM_ANIMATIONS];
 
             //Loop throught all of the 3D frames to create an animation.
-            for(int j = 0; j < NUM_3D_FRAMES; j++) {
+            for(int j = 0; j < animations_numframes; j++) {
 
                 int count = 0;
-                while(count < FRAMES_BEFORE_NEXT)
+                while(count < current_animations_framebeforenext)
                 {
                     //Loop through all 24 2D slices of the 3D frame
                     for(int i = 0; i < 24; i++) {
@@ -260,7 +283,7 @@ int main() {
                                 gpio_set_dir(currentGND, GPIO_IN);
 
                                 //Get Pixel On Off Status
-                                pixelVal = (rotatingcube[j][i][y] >> x) & 1;
+                                pixelVal = (activeFrame[j][i][y] >> x) & 1;
 
                                 //Check GPIO pins for current pixel
                                 currentVCC = gpiomap_flipped[y][16 - x][0];
